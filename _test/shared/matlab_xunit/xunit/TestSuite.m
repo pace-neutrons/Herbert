62ce811d999555916dd52f271bf536e3db7ab78c
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-04 11:58:46 +0000

    Remove extraneous var check

--- _test/shared/matlab_xunit/xunit/TestSuite.m
+++ _test/shared/matlab_xunit/xunit/TestSuite.m
@@ -242,7 +242,7 @@ classdef TestSuite < TestComponent
             %   constructs a TestSuite object containing only the test case
             %   named 'testA' found in the TestCase subclass MyTests.
 
-            if exist(name, 'dir') == 7
+            if exist(name, 'dir')
                 suite = TestSuiteInDir(name);
                 suite.gatherTestCases();
                 return;
