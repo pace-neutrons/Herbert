b51762bc4282567a2163fc519f3b759c14014726
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-04 17:58:14 +0000

    Attempt more complete conversion

--- admin/lint_json.m
+++ admin/lint_json.m
@@ -17,13 +17,12 @@ function [raw, json] = lint_json(filesin, outputfile)
     end
 
     if nargout > 0 % Build raw and json for potential output
-        raw = cell(1, numel(files));
         json = cell(1, numel(files));
-        for i = 1:numel(files)
-            raw{i} = checkcode(files{i});
+        raw = checkcode(files);
+        for i = 1:numel(raw)
             for j = 1:numel(raw{i})
                 raw{i}(j).filename = files{i};
-                json{i} = [json{i},jsonencode(raw{i}(j)), nl];
+                json{i} = [json{i},jsonencode(wng_compat(raw{i}(j))), nl];
             end
         end
     end
@@ -48,7 +47,7 @@ function [raw, json] = lint_json(filesin, outputfile)
             json = '';
             for j = 1:numel(raw)
                 raw(j).filename = files{i};
-                json = [json,jsonencode(raw(j)), nl];
+                json = [json,jsonencode(wng_compat(raw(j))), nl];
             end
             fprintf(fh, '%s', json);
         end
@@ -57,4 +56,20 @@ function [raw, json] = lint_json(filesin, outputfile)
     elseif nargout == 0  % No operation to perform
         error('MATLAB:badargs', 'lint_json called with bad args');
     end
+end
+
+function struc = wng_compat(struc)
+% Parse an mlint error struct into a Jenkins Warnings Next Gen compatible form
+    struc = rename(struc, 'line', 'lineStart');
+    struc.columnStart = struc.column(1);
+    struc.columnEnd = struc.column(2);
+    struc = rmfield(struc,'column');
+    struc.severity = "NORMAL";
+end
+
+function struc = rename(struc, old, new)
+% Rename a structure's field 'old' to 'new'
+% From: https://stackoverflow.com/questions/2733582/how-do-i-rename-a-field-in-a-structure-array-in-matlab
+    [struc.(new)] = struc.(old);
+    struc = rmfield(struc,old);
 end
