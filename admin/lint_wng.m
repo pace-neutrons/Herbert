c31623829d4fefe4faa81314a9062767f91c7183
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-05 12:25:51 +0000

    Make lint_wng separate from lint_json

--- admin/lint_wng.m
+++ admin/lint_wng.m
@@ -1,15 +1,40 @@
 function lint_wng(filesin, outputfile)
+% Use mlint, but convert to a form mimicking that of pep8 for easy parsing by WNG
+%
+% Input:
+% ------
+% filesin              char array OR cell array of char arrays detailing files to parse
+%                          if filesin is empty will recurse from current working directory
+% outputfile           char array of filename to write output to (will overwrite)
+%                          if outputfile is empty will write to stdout
 
-    out = lint_json(filesin,'');
-    fh = fopen(outputfile,'w');
-    if fh == -1
-        error("MATLAB:FileOpenError", "Failed to open file %s", outputfile);
+    if isempty(filesin)
+        % Default to glob all
+        filesin = "**/*.m";
     end
+    if isempty(outputfile) % Default to stdout
+        fh = 1;
+    else % Open file
+        fh = fopen(outputfile,'w');
+        if fh == -1
+            error("MATLAB:FileOpenError", "Failed to open file %s", outputfile);
+        end
+        cleanup = onCleanup(@()(fclose(fh)));
+    end
+
+    files = [];
+    for i = 1:numel(filesin)
+        flist = dir(filesin{i});
+        files = [files; arrayfun(@(file)(fullfile(file.folder, file.name)), flist, 'UniformOutput', false)];
+    end
+
+    [out, filepaths] = checkcode(files, '-id');
     for i = 1:numel(out)
         for j = 1:numel(out{i})
             curr = out{i}(j);
+            curr.fileName = filepaths{i};
             fprintf(fh, "%s:%d:%d: %s %s\n", curr.fileName, curr.line, curr.column(1), curr.id, curr.message);
         end
     end
-    fclose(fh);
+
 end
