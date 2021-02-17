e42d50cd6fd9d968c97fe29a73afce1dad10112f
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-17 16:26:05 +0000

    Only skip merges on non-nightly, non-branch runs

--- cmake/shared/PACE_Version.cmake
+++ cmake/shared/PACE_Version.cmake
@@ -3,11 +3,11 @@ herbert_Version
 -----------------
 
 Build a version release string:
-  <version>[-<date>]-<target>-<matlab>[-<sha>]
+<version>[-<date>]-<target>-<matlab>[-<sha>]
 
- Optional elements are included based on the value of ${PROJECT_NAME}_RELEASE_TYPE
- - Date included for "nightly" builds
- - SHA included for non-"release" builds (i.e. "nightly" or "pull-request")
+Optional elements are included based on the value of ${PROJECT_NAME}_RELEASE_TYPE
+- Date included for "nightly" builds
+- SHA included for non-"release" builds (i.e. "nightly" or "pull-request")
 
 Variables required by the module
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -35,28 +35,33 @@ set(${PROJECT_NAME}_FULL_VERSION "${PROJECT_VERSION}")
 string(TOUPPER "${${PROJECT_NAME}_RELEASE_TYPE}" _release_type)
 
 if("${_release_type}" STREQUAL "NIGHTLY")
-    string(TIMESTAMP _date "%Y%m%d")
-    set(${PROJECT_NAME}_FULL_VERSION "${${PROJECT_NAME}_FULL_VERSION}-${_date}")
+  string(TIMESTAMP _date "%Y%m%d")
+  set(${PROJECT_NAME}_FULL_VERSION "${${PROJECT_NAME}_FULL_VERSION}-${_date}")
 endif()
 
 if(UNIX)
-    set(${PROJECT_NAME}_PLATFORM "linux")
+  set(${PROJECT_NAME}_PLATFORM "linux")
 elseif(WIN32)
-    set(${PROJECT_NAME}_PLATFORM "win64")
+  set(${PROJECT_NAME}_PLATFORM "win64")
 endif()
 
 set(${PROJECT_NAME}_FULL_VERSION "${${PROJECT_NAME}_FULL_VERSION}-${${PROJECT_NAME}_PLATFORM}-${Matlab_RELEASE}")
 
 if(NOT "${_release_type}" STREQUAL "RELEASE")
-    find_package(Git QUIET)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --no-merges -n 1 HEAD
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-        RESULT_VARIABLE _res
-        OUTPUT_VARIABLE GIT_REVISION_SHA
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+  find_package(Git QUIET)
+  if ("${_release_type}" STREQUAL "NIGHTLY" OR "${_release_type}" STREQUAL "BRANCH")
+    set(SKIP_MERGES "")
+  else()
+    set(SKIP_MERGES "--no-merges")
+  endif()
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit ${SKIP_MERGES} -n 1 HEAD
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    RESULT_VARIABLE _res
+    OUTPUT_VARIABLE GIT_REVISION_SHA
+    OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    set(${PROJECT_NAME}_FULL_VERSION "${${PROJECT_NAME}_FULL_VERSION}-${GIT_REVISION_SHA}")
+  set(${PROJECT_NAME}_FULL_VERSION "${${PROJECT_NAME}_FULL_VERSION}-${GIT_REVISION_SHA}")
 endif()
 
 message(STATUS "${PROJECT_NAME}_FULL_VERSION: ${${PROJECT_NAME}_FULL_VERSION}")
@@ -64,5 +69,5 @@ message(STATUS "${PROJECT_NAME}_FULL_VERSION: ${${PROJECT_NAME}_FULL_VERSION}")
 # Set the short version which is used in ${PROJECT_NAME}_version.[m|h] files
 set(${PROJECT_NAME}_SHORT_VERSION "${PROJECT_VERSION}")
 if(NOT "${_release_type}" STREQUAL "RELEASE")
-    set(${PROJECT_NAME}_SHORT_VERSION "${${PROJECT_NAME}_SHORT_VERSION}\.${GIT_REVISION_SHA}")
+  set(${PROJECT_NAME}_SHORT_VERSION "${${PROJECT_NAME}_SHORT_VERSION}\.${GIT_REVISION_SHA}")
 endif()
