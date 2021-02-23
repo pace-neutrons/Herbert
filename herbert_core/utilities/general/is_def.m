8876bd8d1e2cb80e23c19ebdc763a26ca014842b
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-23 23:15:27 +0000

    Remove print

--- herbert_core/utilities/general/is_def.m
+++ herbert_core/utilities/general/is_def.m
@@ -2,6 +2,6 @@ function ok = is_def(name)
 % Alias for exist(name, 'var') == 1
 % For agreement with other PACE "is_" functions
 
-    ok = exist(name, 'var') == 1
+    ok = exist(name, 'var') == 1;
 
 end
