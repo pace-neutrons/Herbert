94d48b3069652a79949cb8112324675ea24f11a7
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-03-03 16:12:29 +0000

    Clarify docs

--- herbert_core/utilities/general/is_file.m
+++ herbert_core/utilities/general/is_file.m
@@ -1,15 +1,12 @@
 function ok = is_file(name)
 % tests if name is a file on the file system while NOT searching MATLAB path
 %
-% Optional support for a cell array of file extensions which name must also match.
-%
 % On older versions of matlab this is done through ensuring the path is an explicit path
 % and using exist. More recent versions simply call the MATLAB built-in isfile.
 %
 % Input:
 % ------
 %   name                The name of the file you want to check;
-%   {'.a','.b','.c'}    List of the permitted extensions
 %
 % Usage:
 % ------
