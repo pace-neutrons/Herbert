979714058d6e99d28c41d37f891186ab5d93a4c9
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-03-22 13:24:47 +0000

    Convert returning ok into modern errors

--- herbert_core/applications/multifit/@mfclass/fit.m
+++ herbert_core/applications/multifit/@mfclass/fit.m
@@ -1,4 +1,4 @@
-function [data_out, fitdata, ok, mess] = fit (obj, varargin)
+function [data_out, fitdata] = fit (obj, varargin)
 % Perform a fit of the data using the current functions and parameter values
 %
 % Return calculated fitted datasets and parameters:
@@ -90,14 +90,6 @@ function [data_out, fitdata, ok, mess] = fit (obj, varargin)
 
 % Default return values if there is an error
 data_out = [];
-fitdata = [];
-
-% Determine if not ok will throw an error
-if nargout<3
-    throw_error = true;
-else
-    throw_error = false;
-end
 
 % Check option
 opt_default = struct('sum',false,'components',false);
@@ -111,57 +103,45 @@ if ok
         elseif sum(lopt)==1
             output_type = flagnames{lopt};
         else
-            ok = false; mess = 'Check the value of output option';
+            error("Herbert:mfclass:badargs", 'Check the value of output option')
         end
     else
-        ok = false; mess = 'Check number of input arguments';
+        error("Herbert:mfclass:badargs", 'Check number of input arguments')
     end
-end
-if ~ok
-    if throw_error, error_message(mess), else, return, end
+else
+    error("Herbert:mfclass:badargs", mess)
 end
 
 % Check that there is data present
 if obj.ndatatot_ == 0
-    ok = false; mess = 'No data has been provided for fitting';
-    if throw_error, error_message(mess), else, return, end
+    error("Herbert:mfclass:badargs", 'No data has been provided for fitting')
 end
 
 % Check that all functions are present
 foreground_present = ~all(cellfun(@isempty,obj.fun_));
 background_present = ~all(cellfun(@isempty,obj.bfun_));
+
 if ~foreground_present && ~background_present
-    ok = false; mess = 'No fit functions have been provided';
-    if throw_error, error_message(mess), else, return, end
+    error("Herbert:mfclass:badargs", 'No fit functions have been provided')
 end
 
 % Mask the data
-[wmask, msk_out, ok, mess] = mask_data_for_fit (obj.w_, obj.msk_);
-if ~ok
-    if throw_error, error_message(mess), else, return, end
-end
+[wmask, msk_out] = mask_data_for_fit(obj.w_, obj.msk_);
 
 % Check that there are parameters and unmasked data to be fitted
-[~, ok, mess, pfin, p_info] = ptrans_initialise_ (obj);
+[~, ok, mess, pfin, p_info] = ptrans_initialise_(obj);
 if ~ok
-    if throw_error, error_message(mess), else, return, end
+    error("Herbert:mfclass:badtransinit", mess)
 end
 
 % Allow for the case of input argument over-riding initial parameter values for fit
 if numel(args)==1
-    [pfin,ok_sim,mess] = ptrans_par_inverse(args{1}, p_info);
-    if ~ok_sim
-        ok = false;
-        if throw_error, error_message(mess), else, return, end
-    end
+    pfin = ptrans_par_inverse(args{1}, p_info);
 end
 
 % Get wrapped functions and parameters after performing initialisation if required
-[ok, mess, fun_wrap, pin_wrap, bfun_wrap, bpin_wrap] = ...
+[fun_wrap, pin_wrap, bfun_wrap, bpin_wrap] = ...
     wrap_functions_and_parameters (obj.wrapfun_, wmask, obj.fun_, obj.pin_, obj.bfun_, obj.bpin_);
-if ~ok
-    if throw_error, error_message(mess), else, return, end
-end
 
 % Now fit the data
 xye = cellfun(@isstruct, obj.w_);
@@ -171,12 +151,9 @@ listing = obj.options_.listing;
 fcp = obj.options_.fit_control_parameters;
 perform_fit = true;
 
-[pf, sig, cor, chisqr_red, converged, ok, mess] =...
+[pf, sig, cor, chisqr_red, converged] =...
     multifit_lsqr (wmask, xye, fun_wrap, bfun_wrap, pin_wrap, bpin_wrap,...
     f_pass_caller, bf_pass_caller, pfin, p_info, listing, fcp, perform_fit);
-if ~ok
-    if throw_error, error_message(mess), else, return, end
-end
 
 % Evaluate the data at the fitted parameter values
 %  On the face of it, it should not be necessary to re-evaluate the function,
@@ -212,11 +189,8 @@ if selected
 else
     % Need to re-initialise because data is unmasked i.e. not the same as fitted
     % (if there is no initialisation to be done, then cheap call)
-    [ok, mess, fun_wrap, pin_wrap, bfun_wrap, bpin_wrap] = ...
+    [fun_wrap, pin_wrap, bfun_wrap, bpin_wrap] = ...
         wrap_functions_and_parameters (obj.wrapfun_, obj.w_, obj.fun_, obj.pin_, obj.bfun_, obj.bpin_);
-    if ~ok
-        if throw_error, error_message(mess), else, return, end
-    end
 
     % Now compute output
     wout = multifit_func_eval (obj.w_, xye, fun_wrap, bfun_wrap, pin_wrap, bpin_wrap,...
@@ -236,4 +210,4 @@ end
 % Package output fit results
 fitdata = repackage_output_parameters (pf, sig, cor, chisqr_red, converged, p_info,...
     foreground_present, background_present);
-
+end
