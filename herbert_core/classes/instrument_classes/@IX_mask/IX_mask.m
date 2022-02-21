22b266c79cb394f8bc05cf750167dc02d214a213
Toby Perring <toby.perring@stfc.ac.uk>
2022-02-21 12:06:33 +0000

    Start refactoring of IX_mask

--- herbert_core/classes/instrument_classes/@IX_mask/IX_mask.m
+++ herbert_core/classes/instrument_classes/@IX_mask/IX_mask.m
@@ -1,59 +1,71 @@
-function wout=IX_mask(varargin)
-% Constructor for IX_map object. There are numerous ways to specify the map
-%
-%   >> w = IX_mask(filename)        % Read from ascii file
-%   >> w = IX_mask(iarray)          % Array of spectra to be masked
-%
-% A mask object contains a list of spectra to be masked, where all spectrum
-% numbers are greater than or equal to one. The array is sorted into
-% numerically increasing order, with duplicates removed.
-
-% Original author: T.G.Perring
-
-classname='IX_mask';
-
-% Catch default constructor or existing IX_map
-% --------------------------------------------
-if nargin==0
-    % Default constructor: empty array
-    wout.msk=zeros(1,0);
-    [ok,mess,wout]=checkfields(wout);   % Make checkfields the ultimate arbiter of the validity of a structure
-    if ok, wout = class(wout,classname); return, else error(mess); end
+classdef IX_mask
+    % IX_mask   Definition of mask class
     
-elseif nargin==1 && isa(varargin{1},classname)
-    % Is a mask object already
-    wout = varargin{1};
-    return
-end
-
-
-% Handle other cases
-% ------------------
-if nargin==1
-    if isstruct(varargin{1})
-        % Structure with the fields of a mask object is permitted
-        wout = varargin{1};
-        
-    elseif is_string(varargin{1})
-        % File name input
-        if ~isempty(varargin{1})
-            [wout,ok,mess]=get_mask(varargin{1});
-            if ~ok, error(mess), end
-        else
-            error('File name cannot be an empty string')
-        end
-        
-    elseif isnumeric(varargin{1}) && all(rem(varargin{1}(:),1)==0) && all(varargin{1})>0
-        wout.msk=varargin{1};
-        
-    else
-        error('Input argument must be an array of positive integers or a file name')
+    properties
+        % Spectra to be masked. Row vector of integers greater than zero
+        msk = ones(1,0)
     end
     
-else
-    error('Check number and/or type of arguments')
+    methods
+        %------------------------------------------------------------------
+        % Constructor
+        %------------------------------------------------------------------
+        function obj = IX_mask (val)
+            % Create IX_mask object.
+            %
+            %   >> obj = IX_mask (iarray)       % Array of spectra
+            %   >> obj = IX_mask (filename)     % Read arrays from ascii file
+            %
+            % A mask object contains a list of spectra to be masked, where
+            % all spectrum numbers are greater than or equal to one. The
+            % array is sorted into numerically increasing order, with all
+            % duplicates removed.
+            %
+            % Input:
+            % ------
+            %   iarray      Array of spectra to be masked
+            %
+            % *OR*
+            %   filename    Name of ASCII file with list of spectra to mask
+            %               The file can have comment lines indicated by %, !
+            %              or blank lines
+            %               Spectrum numbers are indicated by the contents
+            %              of any valid matlab array constructor (i.er. with
+            %              the leading '[' and closing ']' missing
+            %
+            %               EXAMPLE:
+            %                   % A comment line
+            %                   1:10, 15:20
+            %
+            %                   % Another comment line
+            %                   11:3:35      % an in-line comment
+            
+            
+            if isnumeric(val)
+                % Numeric array
+                msk = unique(val(:)');
+                if ~(any(msk<1) || any(~isfinite(msk)))
+                    obj.msk = msk;
+                else
+                    error ('HERBERT:IX_mask:invalid_argument',...
+                        'Spectrum numbers must be finite and greater or equal to 1')
+                end
+                
+            elseif is_string (val)
+                % Assume a filename
+                if ~isempty(val)
+                    [wout,ok,mess] = get_mask(val);
+                else
+                    error('File name cannot be an empty string')
+                end
+                
+            elseif ~isempty(val)
+                % Unrecognised input
+                error ('HERBERT:IX_mask:invalid_argument',...
+                    'Input must be an array or file name')
+            end
+            
+        end
+    end
     
 end
-
-[ok,mess,wout]=checkfields(wout);   % Make checkfields the ultimate arbiter of the validity of a structure
-if ok, wout = class(wout,classname); return, else error(mess); end
